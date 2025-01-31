7b6b899b9cfc9748a98b484c14c980707ee19c41
Reilan Cadubla <reilancadubla@gmail.com>
2025-01-31 13:25:38 +0800

    1st creation for reilandev branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post.create.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post-create/post.create.component.ts
+++ src/app/posts/post-create/post.create.component.ts
@@ -1,3 +1,15 @@
+import { Component } from '@angular/core';
 
-
-export class 
+@Component({
+  selector: 'post-create',
+  templateUrl: './post.create.component.html',
+  styleUrls: ['./post.create.component.css'],
+})
+export class PostCreateComponent {
+enteredValue = ''
+newPost=''
+  onAddPost(){
+    this.newPost = this.enteredValue
+    console.log("heajkabjk")
+  }
+}
