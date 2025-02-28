efd51ab4423bc10674bf346d056ace9c89469ffa
Reilan Cadubla <reilancadubla@gmail.com>
2025-02-28 13:39:21 +0800

    with body parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,20 @@
 import { Component } from '@angular/core';
 
-interface Post {
-  // Define the structure of your Post object
-  id: number;
-  title: any;
-  content: any;
-}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'it_elec_6a'; 
-  storedPosts: Post[] = [];
-  onPostAdded(post: any): void{
-    this.storedPosts.push(post);
-  }  
-  // storedPosts = [];
 
+  // onPostAdded(post: any): void{
+  //   this.storedPosts.push(post);
+  // }  
+  // storedPosts = [];
   // onPostAdded(post){
   //   this.storedPosts.push(post)
   // }
-
 }
